2a2a45091c1c6f9ea4b4cb0d60923f5e65c22f22
Chris Blake <chrisrblake93@gmail.com>
2024-03-09 17:43:49 -0600

    fix: get things running again
    Yay, quartzpro64 is happy finally.

--- scripts/docker/run_debootstrap.sh
+++ scripts/docker/run_debootstrap.sh
@@ -45,7 +45,7 @@ console-common	console-data/keymap/full	select	us
 cp -r ${build_path}/kernel ${build_path}/rootfs/root/
 
 # Remove the debug kernel
-rm ${build_path}/rootfs/root/kernel/linux-image-*-dbg_*.deb
+#rm ${build_path}/rootfs/root/kernel/linux-image-*-dbg_*.deb
 
 # Kick off bash setup script within chroot
 cp ${docker_scripts_path}/bootstrap/001-bootstrap ${build_path}/rootfs/bootstrap
